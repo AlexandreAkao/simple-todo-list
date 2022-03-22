438359ee523348b9fcc0573848e6aaab9b5e4ede
Alexandre Akira <alexandreakiraakao@gmail.com>
2022-03-22 16:29:49 -0300

    feat: add new tasks on list

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -5,23 +5,33 @@ import './styles.css';
 
 function Main() {
   const [newTask, setNewTask] = useState('');
-  const [tasks] = useState([]);
+  const [tasks, setTasks] = useState([]);
 
   const handleNewTask = (event) => {
     setNewTask(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newTaskParsed = newTask.trim();
+
+    if (!tasks.includes(newTaskParsed)) {
+      setTasks((prevTasks) => [...prevTasks, newTaskParsed]);
+      setNewTask('');
+    }
   };
 
   return (
     <div className="main">
       <h1>Lista de tarefas</h1>
 
-      <form action="#" className="form">
+      <form onSubmit={handleSubmit} className="form">
         <input value={newTask} type="text" onChange={handleNewTask} />
         <button type="submit">
           <FaPlus />
         </button>
       </form>
-      <ul className="tarefas">
+      <ul className="tasks">
         {tasks.map((tarefa) => (
           <li key={tarefa}>
             {tarefa}
